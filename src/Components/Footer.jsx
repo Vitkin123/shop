1f5d554c5957214f95552e03eb01950b751757b1
Yaroslav Vitkin <vitkin@ex.ua>
2021-03-16 19:57:47 +0200

    with repo ref

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,12 @@ export const Footer = () => {
             <div className="footer-copyright teal darken-4">
                 <div className="container">
                     {new Date().getFullYear()}
-                    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                    <a className="grey-text text-lighten-4 right"
+                       href="https://github.com/Vitkin123/shop"
+                       rel="noreferrer"
+                       target="_blank"
+
+                    >Repo</a>
                 </div>
             </div>
         </footer>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,9 +4,6 @@ export const Header = () => {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {/*<li><a href="sass.html">Sass</a></li>*/}
-                    {/*<li><a href="badges.html">Components</a></li>*/}
-                    {/*<li><a href="collapsible.html">JavaScript</a></li>*/}
                 </ul>
             </div>
         </nav>
